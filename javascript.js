26617a1dbaa0bc238ded8e750eb0cf5ae9f11872
Armin <rmeen.shokati@gmail.com>
2019-02-20 18:55:11 +0330

    commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+var str = 'Hello World';
+
+function sayHello() {
+	alert(str);
+}
+
+function comp() {
+	if(2 == '2'){
+		//true
+		//
+	}
+	if (2 === '2'){
+		//false
+		
+	}
+}
+
+var func = function funcToVar() {
+	alert("I'm function")
+}
+
+func();
